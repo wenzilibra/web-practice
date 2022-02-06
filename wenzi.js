be85347d67db9c2bf1c5d8a1476df1b32f85a0c6
wenzilibra <413335430@qq.com>
2022-02-06 23:28:02 +0800

    if else 分支语句

--- wenzi.js
+++ wenzi.js
@@ -1,5 +1,12 @@
-alert('走过路过别错过');
+/* alert('走过路过别错过');
 console.log('xixi');
 var myName = prompt ('请输入姓名');
 console.log(myName);
-alert('欢迎' + myName + '登录呀');
+alert('欢迎' + myName + '登录呀'); */
+
+var youName = prompt('输入你的姓名');
+if (youName == '刘德华') {
+    alert('恭喜你中得5元');
+} else {
+    alert('很遗憾你没有中奖');
+}
